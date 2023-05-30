c3ae663487f24dc7a2b965d0d471c7b8642a46b9
Simon Gaubert <sim.gaubert.sg@gmail.com>
2023-05-30 02:48:29 +0200

    improve token

--- c++/src/token.cpp
+++ c++/src/token.cpp
@@ -3,8 +3,31 @@
 using namespace std;
 
 string tokenTypeToStr(TokenType type) {
-	string tab[] = { "PLUS", "MINUS", "MULT", "DIV", "MOD", "LPAR", "RPAR", "LHOOK", "RHOOK", "COMMA", "SEMICOLON", "EQUAL", "QUESTION", "POWER", "MATMULT", "I", "VAR", "NUMBER", "SUBTREE", "NONE" };
-	return tab[int(type)];
+	string str = "";
+	switch (type) {
+		case OPERATOR : { str = "OPERATOR"; break; }
+		case PLUS : { str = "PLUS"; break; }
+		case MINUS : { str = "MINUS"; break; }
+		case MULT : { str = "MULT"; break; }
+		case DIV : { str = "DIV"; break; }
+		case MOD : { str = "MOD"; break; }
+		case POWER : { str = "POWER"; break; }
+		case MATMULT : { str = "MATMULT"; break; }
+		case OPERAND : { str = "OPERAND"; break; }
+		case I : { str = "I"; break; }
+		case VAR : { str = "VAR"; break; }
+		case NUMBER : { str = "NUMBER"; break; }
+		case LPAR : { str = "LPAR"; break; }
+		case RPAR : { str = "RPAR"; break; }
+		case LHOOK : { str = "LHOOK"; break; }
+		case RHOOK : { str = "RHOOK"; break; }
+		case COMMA : { str = "COMMA"; break; }
+		case SEMICOLON : { str = "SEMICOLON"; break; }
+		case EQUAL : { str = "EQUAL"; break; }
+		case QUESTION : { str = "QUESTION"; break; }
+		case SUBTREE : { str = "SUBTREE"; break; }
+		case NONE : { str = "NONE"; break; }
+	}
 }
 
 // class Token
